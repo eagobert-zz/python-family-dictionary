24687f0b200b6373b2d0c31e136be12d845664b5
eagobert <eagobert@hotmail.com>
2018-07-06 15:36:28 -0500

    yes.  finally got the hang of nested loops in dict comprehension

--- family_dict.py
+++ family_dict.py
@@ -1,5 +1,6 @@
-#Define a dictionary that contains information about several members of your family. Use the following example as a template.
+#Define a dictionary that contains information about several members of your family.
 
+#Create a dictionary called "my_family" with the keys: mother and father, which hold dictionary objects in their values. 
 my_family = {
     "mother": {
         "name": "Earline Agobert",
@@ -11,6 +12,29 @@ my_family = {
     }
 }
 
+# print the entire dictionary
 print(my_family)
 
-#Using a dictionary comprehension, produce output that looks like the following example.
+#loop thru dictionary and print for each key
+for k, v in my_family.items():
+    print("My " + k + " is " + v["name"])
+
+#print the value of a specific key in the dictionary
+print(my_family["mother"])
+print(my_family["father"])
+
+#print all keys in a dictionary
+print("Keys: ", my_family.keys())
+
+# print all values in a dictionary
+print("Values: ", my_family.values())
+
+#print all keys and values in a dictionary
+print("Keys and Values: ", my_family.items())
+
+#Using a dictionary comprehension, produce output that looks like the following example: "Krista is my sister and is 42 years old."
+family = { value["name"] + " is my " + key + " and is " + str(value["age"]) + " years old." for (key, value) in my_family.items()}
+
+print(family)
+
+
