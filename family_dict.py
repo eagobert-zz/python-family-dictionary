23af64250871d6d7c9fa64e5b01a3d618ac76cb2
eagobert <eagobert@hotmail.com>
2018-07-06 10:44:26 -0500

    Initial commit

---
+++ family_dict.py
@@ -0,0 +1,16 @@
+#Define a dictionary that contains information about several members of your family. Use the following example as a template.
+
+my_family = {
+    "mother": {
+        "name": "Earline Agobert",
+        "age": 58
+    },
+    "father": {
+        "name": "Dwain Agobert",
+        "age": 60
+    }
+}
+
+print(my_family)
+
+#Using a dictionary comprehension, produce output that looks like the following example.
